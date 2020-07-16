bb7c6e812cb0af516afccab1e8b709dd1bcc7ead
Jack Faller <jack.t.faller@gmail.com>
2020-07-17 00:33:10 +0100

    added message for removed files

--- nvim/histclear.sh
+++ nvim/histclear.sh
@@ -1,7 +1,9 @@
 histfiles=`ls ~/.config/nvim/histfiles`
+echo "related file/s do not exist, moving to /tmp:"
 for histfile in $histfiles
 do
         if ! test -f `echo $histfile | sed 's#%#/#g'`; then
                 mv "$HOME/.config/nvim/histfiles/$histfile" /tmp
+                echo "    $histfile"
         fi
 done

f9d91133880f8c8800340dc28629d0113969ee0d
Jack Faller <jack.t.faller@gmail.com>
2021-02-09 12:58:40 +0000

    added comment

--- qutebrowser/get.sh
+++ qutebrowser/get.sh
@@ -2,7 +2,7 @@
 
 . "$HOME/.cache/wal/colors.sh"
 line=$(cat ~/.config/qutebrowser/stowed | sort | dmenu -i -b -l 20 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15")
-url=$(echo "$line" | sed 's/.* //g')
+url=$(echo "$line" | sed 's/.* //g') #strip tab title
 
 if [ $(grep -x -v "$line" "$QUTE_CONFIG_DIR/stowed") = "" ]; then #don't open tabs not in file
     exit

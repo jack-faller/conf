3d3439829a738f4392f8e02e6e6a4b9748165517
Jack Faller <jack.t.faller@gmail.com>
2021-02-11 18:01:56 +0000

    editor now loads colors

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,3 +5,4 @@ config.bind(':', 'set statusbar.show always;; set-cmd-text :')
 config.bind('/', 'set statusbar.show always;; set-cmd-text /')
 config.bind('<Escape>', 'mode-enter normal;; set statusbar.show in-mode', mode='command')
 config.bind('<Return>', 'command-accept;; set statusbar.show in-mode', mode='command')
+c.editor.command = ["alacritty", "-e", "zsh", "-c", "cat ~/.cache/wal/sequences ; nvim {file} -c \"normal {line}G{column0}l\""]

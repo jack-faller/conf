03d3163661d3b4b091e1056a2fc917c7bc9f5654
Jack Faller <jack.t.faller@gmail.com>
2021-07-08 18:32:20 +0100

    more stuff, fixed binds

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,8 @@
 (menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
+(toggle-scroll-bar -1) (tool-bar-mode -1)
+
+(setq show-paren-delay 0)
+(show-paren-mode)
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -44,9 +46,45 @@
      (evil-search-module 'evil-search)
      (evil-undo-system 'undo-tree)
      (evil-set-undo-system 'undo-tree)
-     (evil-want-fine-undo t)
      (evil-ex-substitute-global t)
+     (setq evil-emacs-state-cursor 'box
+	   evil-normal-state-cursor 'box
+	   evil-visual-state-cursor 'box
+	   evil-insert-state-cursor 'bar
+	   evil-replace-state-cursor 'hbar
+	   evil-operator-state-cursor 'hollow)
      :config
+     (setq evil-extra-operator-eval-modes-alist
+	   '((lisp-mode slime-eval-region)
+	     (scheme-mode geiser-eval-region)
+	     (clojure-mode cider-eval-region)
+	     (ruby-mode ruby-send-region)
+	     (enh-ruby-mode ruby-send-region)
+	     (python-mode python-shell-send-region)
+	     (julia-mode julia-shell-run-region)))
+     (evil-define-operator evil-eval (beg end)
+       "Evil operator for evaluating code."
+       :move-point nil
+       (interactive "<r>")
+       (let* ((ele (assoc major-mode evil-extra-operator-eval-modes-alist))
+	      (f-a (cdr-safe ele))
+	      (func (car-safe f-a))
+	      (args (cdr-safe f-a)))
+	 (if (fboundp func)
+	     (apply func beg end args)
+	   (eval-region beg end t))))
+     (evil-define-operator evil-replace-with-reg (beg end register)
+       "Evil operator for evaluating code."
+       (interactive "<r><x>")
+       (evil-delete beg end :register ?_)
+       (evil-paste-before 1 register))
+     (evil-define-key 'motion 'global (kbd "M-e") 'evil-backward-word-end)
+     (evil-define-key 'motion 'global (kbd "M-E") 'evil-backward-WORD-end)
+     (evil-define-key 'normal 'global (kbd "ge") 'evil-eval)
+     (evil-define-key 'visual 'global (kbd "ge") 'evil-eval)
+     (evil-define-key 'normal 'global (kbd "gs") 'evil-replace-with-reg)
+     (evil-define-key 'visual 'global (kbd "gs") 'evil-replace-with-reg)
+     (evil-define-key 'normal 'global (kbd "C-u") 'evil-scroll-up)
      (evil-mode 1))
 
 (pkg auto-package-update
@@ -57,11 +95,15 @@
      (auto-package-update-maybe))
 
 (pkg evil-surround
-  :config
-  (global-evil-surround-mode 1))
+	   :config
+	   (global-evil-surround-mode 1))
 
-(pkg targets "noctuid/targets.el"
-     :config (targets-setup))
+(pkg-github targets "noctuid/targets.el"
+	    :config
+	    (targets-setup t))
+
+(pkg evil-exchange
+     :config (evil-exchange-install))
 
 ; centre line on scroll
 (add-hook 'post-command-hook #'recenter)
@@ -70,31 +112,56 @@
      :after evil
      :custom
      (evil-collection-setup-minibuffer t)
-     :init
+     :config
+     (setq evil-collection-mode-list (delete 'lispy evil-collection-mode-list))
      (evil-collection-init))
 
 (pkg lispy
-     :config
-     (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1))))
+     :after evil-collection
+     :init
+     (add-hook 'emacs-lisp-mode-hook 'lispy-mode)
+     (add-hook 'common-lisp-mode-hook 'lispy-mode)
+     (add-hook 'scheme-mode-hook 'lispy-mode)
+     (add-hook 'lisp-mode-hook 'lispy-mode))
+
 (pkg lispyville
      :init
-     (add-hook 'emacs-lisp-mode-hook #'lispyville-mode)
-     (add-hook 'lisp-mode-hook #'lispyville-mode)
+     (add-hook 'lispy-mode-hook
+               (lambda ()
+		 (lispyville-mode)
+		 (targets-define-to lispyville-comment 'lispyville-comment nil object
+				    :bind t :keys   "c")
+		 (targets-define-to lispyville-atom 'lispyville-atom nil object
+				    :bind t :keys "a")
+		 (targets-define-to lispyville-list 'lispyville-list nil object
+				    :bind t :keys "f")
+		 (targets-define-to lispyville-sexp 'lispyville-sexp nil object
+				    :bind t :keys "x")
+		 (targets-define-to lispyville-function 'lispyville-function nil object
+				    :bind t :keys "d")
+		 (targets-define-to lispyville-comment 'lispyville-comment nil object
+				    :bind t :keys "c")
+		 (targets-define-to lispyville-string 'lispyville-string nil object
+				    :bind t :keys "s")))
+
      :config
-     (lispyville-set-key-theme '(operators c-w additional)))
+     (lispyville-set-key-theme '(operators
+				c-w
+				prettify
+				additional-movement
+				commentary
+				slurp/barf-cp
+				additional
+				(escape insert))))
 
 (add-to-list 'default-frame-alist '(font . "Iosevka 9"))
 (set-face-attribute 'default t :font "Iosevka")
 
 (global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
+(setq-default display-line-numbers t
+              display-line-numbers-widen t
+	      display-line-numbers-type 'relative)
 
-(setq evil-emacs-state-cursor 'box
-      evil-normal-state-cursor 'box
-      evil-visual-state-cursor 'box
-      evil-insert-state-cursor 'bar
-      evil-replace-state-cursor 'bar
-      evil-operator-state-cursor 'hollow)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -103,8 +170,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(centered-scroll lispyville evil-surround lispy evil)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ) 
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )

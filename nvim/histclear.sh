3aaabc13876007dc73e62c74812f89266edce703
Jack Faller <jack.t.faller@gmail.com>
2022-09-12 21:12:25 +0100

    Removes nvim stuff

--- nvim/histclear.sh
+++
@@ -1,9 +0,0 @@
-histfiles=`ls ~/.config/nvim/histfiles`
-echo "related file/s do not exist, moving to /tmp:"
-for histfile in $histfiles
-do
-        if ! test -f `echo $histfile | sed 's#%#/#g'`; then
-                mv "$HOME/.config/nvim/histfiles/$histfile" /tmp
-                echo "    $histfile"
-        fi
-done
